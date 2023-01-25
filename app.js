4b8eae075207eb92ea002ba9f062da93b9890740
wahyou <wahyukrisnaaji32@gmail.com>
2023-01-25 16:53:00 +0700

    update food menu

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const menu = ['ayam geprek', 'soto ayam', 'nasi goreng', 'mie goreng'];
+const menu = ['ayam geprek', 'soto ayam', 'nasi goreng', 'mie goreng', 'sate', 'nasi campur', 'ayam goreng', 'ayam krispi indomaret', 'onigiri', 'ayam goreng'];
 
 function chooseMeal(choices) {
   const selectedMeal = choices[Math.floor(Math.random() * choices.length)];
